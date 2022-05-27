010588844646eaaabd59a71f3d74fea08d14519a
Abror004 <abrorrajabov004@gmail.com>
2022-05-28 00:07:46 +0500

    xdmdss

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
 
 class Home_Page extends StatefulWidget {
+  const Home_Page({Key? key}) : super(key: key);
   static const String id = "home_page";
 
   @override
@@ -8,21 +14,86 @@ class Home_Page extends StatefulWidget {
 }
 
 class _Home_PageState extends State<Home_Page> {
+  // List list = ["assets/file/abror.txt","assets/file/flutter.txt","assets/file/info.txt"];
+  // String a = "";
+  //
+  //
+  //
+  // void sssss (int s) async {
+  //   a = await rootBundle.loadString(list[s]);
+  // }
+  //
+  // Future<String> getImageFileFromAssets(String path) async {
+  //   final byteData = await rootBundle.load('assets/$path');
+  //   final file = File('${(await getTemporaryDirectory()).path}/$path');
+  //   await file.create();
+  //   await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  //   String a = await rootBundle.loadString(file.path);
+  //   return a;
+  // }
+  //
+  // void clear() {
+  //   a = "";
+  // }
+  List list = ["assets/file/abror.pdf"];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(5),
-        child: TextField(
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: "text",
-          ),
-          keyboardAppearance: Brightness.dark,
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("",style: TextStyle(fontSize: 20),),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const SizedBox(
+                      height: 50,
+                      width: 100,
+                      child: ColoredBox(
+                        color: Colors.green,
+                        child: Center(
+                          child: Text("load file"),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Divider(),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: SizedBox(
+                      height: 50,
+                      width: 100,
+                      child: ColoredBox(
+                        color: Colors.green,
+                        child: Center(
+                          child: Text("clear"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
