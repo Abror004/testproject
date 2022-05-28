2e275a9263ffe83b9b9f2915b5b70f8912ab32de
Abror004 <abrorrajabov004@gmail.com>
2022-05-28 14:08:58 +0500

    pdf to text test version app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:soundtest/service/pdftotext_service.dart';
 
 
 class Home_Page extends StatefulWidget {
@@ -35,65 +35,127 @@ class _Home_PageState extends State<Home_Page> {
   // void clear() {
   //   a = "";
   // }
-  List list = ["assets/file/abror.pdf"];
 
+  String text = "";
+  File? file;
+  bool isLoading = false;
+  bool ethernet = true;
+
+  void loadText() async {
+        isLoading = true;
+        bool hasFile = await openPDF();
+        print("+++++++++>>> pdf olindi: $hasFile");
+        if(hasFile) {
+          try{
+            text = await Network.paramsLoad(file!);
+            ethernet = true;
+          } on  SocketException catch (e) {
+            print("no ethernet: ${e}");
+            ethernet = false;
+          }
+        }
+        print("&&&&: ${text}");
+        setState(() {
+          isLoading = false;
+        });
+        file = null;
+        print("++++++++++++++++++= tugadi +++++++++++++++");
+  }
+
+  Future<bool> openPDF() async {
+    print(".");
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    print("..");
+    if (result != null) {
+      setState(() {
+        file = File(result.files.single.path!);
+      });
+      print("...true");
+      return true;
+    }
+    print("...false");
+    return false;
+  }
+
+  void checkEthernet() async {
+      try {
+        await Network.paramsLoad(File("assets/file/salom.pdf"));
+      } on  SocketException catch (e) {
+        ethernet = false;
+      }
+      ethernet = true;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isLoading = true;
+    checkEthernet();
+    isLoading = false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("",style: TextStyle(fontSize: 20),),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: const SizedBox(
-                      height: 50,
-                      width: 100,
-                      child: ColoredBox(
-                        color: Colors.green,
-                        child: Center(
-                          child: Text("load file"),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Divider(),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: SizedBox(
-                      height: 50,
-                      width: 100,
-                      child: ColoredBox(
-                        color: Colors.green,
-                        child: Center(
-                          child: Text("clear"),
-                        ),
-                      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              InkWell(
+                onTap: () {
+                  checkEthernet();
+                  loadText();
+                },
+                child: const SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: ColoredBox(
+                    color: Colors.green,
+                    child: Center(
+                      child: Text("load file"),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 40,),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    text = "";
+                  });
+                },
+                child: SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: ColoredBox(
+                    color: Colors.green,
+                    child: Center(
+                      child: Text("clear"),
+                    ),
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              ethernet ? isLoading ? Center(child: CircularProgressIndicator(),)
+                  : textWidget()
+                  : Column(
+                children: [
+                  Text("No Ethernet"),
+                  SizedBox(height: 10,),
+                  Icon(Icons.signal_wifi_off,color: Colors.red,size: 50,),
+                ],)
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget textWidget() {
+    return Text(text, style: TextStyle(color: Colors.black),);
   }
 }
